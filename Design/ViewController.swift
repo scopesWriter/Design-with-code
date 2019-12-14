1719c2a9c08aaa2d20af26feed3c34a0fbbacccd
bishoy wadie <bishoywadie@bishoys-mac.local>
2019-12-14 08:40:03 -0800

    Initial Commit

---
+++ Design/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Design
+//
+//  Created by bishoy wadie on 12/14/19.
+//  Copyright © 2019 bishoy wadie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
